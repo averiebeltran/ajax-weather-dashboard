1511a09ccdd512e1f8b48f543aea2c3b71238bf3
AverieBeltran <averiebeltran@github.com>
2021-01-14 21:42:14 -0600

    got current date, ajax for 5 day forecast

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,14 +13,24 @@ $( document ).ready(function() {
         data: 'json',
         success: (response) => {
             console.log(response);
-            $(".city").html(response.name);
-            $(".temp").html(response.main.temp);
-            $(".humidity").html(response.main.humidity);
-            $(".wind-speed").html(response.wind.speed);
+            const icon = response.weather[0].icon;
+            const iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
+            const timestamp = response.dt;
+            const date = new Date(timestamp * 1000);
+            const month = (date.getMonth() + 1);
+            const day = (date.getDate());
+            const year = (date.getFullYear());
+            const currentDay = month + '/' + day + '/' + year;
+
+            $(".city").html(response.name + ' ' + currentDay + "<img src=" + iconurl + ">");
+            $(".temp").html("Temperature: " + response.main.temp);
+            $(".humidity").html("Humidity: " + response.main.humidity);
+            $(".wind-speed").html("Wind Speed: " + response.wind.speed);
 
             const lat = response.coord.lat;
             const lon = response.coord.lon;
-            const uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat +"&lon=" + lon + "&appid=c2a1f6a8ab689e2fe8baffb9baf0d523";
+            const uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=c2a1f6a8ab689e2fe8baffb9baf0d523";
+            const fiveDayUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&appid=c2a1f6a8ab689e2fe8baffb9baf0d523"
 
             $.ajax({
                 method: "GET",
@@ -28,7 +38,31 @@ $( document ).ready(function() {
                 data: 'json',
                 success: (response) => {
                     console.log(response);
-                    $(".uv").html(response.value);
+                    $(".uv").html("UV: " + response.value);
+                }
+            });
+
+            $.ajax({
+                method: "GET",
+                url: fiveDayUrl,
+                data: 'json',
+                success: (response) => {
+                    console.log(response);
+                    const forecastDays = response.daily;
+
+                    jQuery.each(forecastDays, function(index, value) {
+                        const forecastTimestamp = value.dt;
+                        const forecastDate = new Date(forecastTimestamp * 1000);
+                        const forecastMonth = (forecastDate.getMonth() + 1);
+                        const forecastDay = (forecastDate.getDate());
+                        const forecastYear = (forecastDate.getFullYear());
+                        const formattedDay = forecastMonth + '/' + forecastDay + '/' + forecastYear;
+
+                        if (index < 6 && index > 0) {
+                            console.log(formattedDay);
+                            
+                        }
+                    });
                 }
             });
         }
