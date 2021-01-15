d2a0a3cc099f0e0f79873be34647a853ecb06381
AverieBeltran <averiebeltran@github.com>
2021-01-14 20:34:56 -0600

    ajax calls working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,36 @@
 $( document ).ready(function() {
+    const data = {
+        city: 'Austin'
+    }
+
+    const city = "austin";
+
+    const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c2a1f6a8ab689e2fe8baffb9baf0d523";
+
     $.ajax({
         method: "GET",
-        url: "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=c2a1f6a8ab689e2fe8baffb9baf0d523",
-        dataType: "json"
-      })
-        .done(function( msg ) {
-          alert( "Data Saved: " + msg );
-        });
+        url: weatherUrl,
+        data: 'json',
+        success: (response) => {
+            console.log(response);
+            $(".city").html(response.name);
+            $(".temp").html(response.main.temp);
+            $(".humidity").html(response.main.humidity);
+            $(".wind-speed").html(response.wind.speed);
+
+            const lat = response.coord.lat;
+            const lon = response.coord.lon;
+            const uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat +"&lon=" + lon + "&appid=c2a1f6a8ab689e2fe8baffb9baf0d523";
+
+            $.ajax({
+                method: "GET",
+                url: uvUrl,
+                data: 'json',
+                success: (response) => {
+                    console.log(response);
+                    $(".uv").html(response.value);
+                }
+            });
+        }
+    });
 });
