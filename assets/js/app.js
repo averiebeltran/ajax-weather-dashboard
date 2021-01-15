fb0741f05aa48b0bfd70586810ca412f6012d564
AverieBeltran <averiebeltran@github.com>
2021-01-14 22:40:07 -0600

    local storage, append, list items.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,70 +1,105 @@
 $( document ).ready(function() {
-    const data = {
-        city: 'Austin'
+    const searches = [];
+    const keys = Object.keys(localStorage);
+    console.log(searches);
+    let i = keys.length;
+
+    while (i--) {
+        searches.push(localStorage.getItem(keys[i]));
     }
 
-    const city = "austin";
-
-    const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c2a1f6a8ab689e2fe8baffb9baf0d523";
-
-    $.ajax({
-        method: "GET",
-        url: weatherUrl,
-        data: 'json',
-        success: (response) => {
-            console.log(response);
-            const icon = response.weather[0].icon;
-            const iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
-            const timestamp = response.dt;
-            const date = new Date(timestamp * 1000);
-            const month = (date.getMonth() + 1);
-            const day = (date.getDate());
-            const year = (date.getFullYear());
-            const currentDay = month + '/' + day + '/' + year;
-
-            $(".city").html(response.name + ' ' + currentDay + "<img src=" + iconurl + ">");
-            $(".temp").html("Temperature: " + response.main.temp);
-            $(".humidity").html("Humidity: " + response.main.humidity);
-            $(".wind-speed").html("Wind Speed: " + response.wind.speed);
-
-            const lat = response.coord.lat;
-            const lon = response.coord.lon;
-            const uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=c2a1f6a8ab689e2fe8baffb9baf0d523";
-            const fiveDayUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&appid=c2a1f6a8ab689e2fe8baffb9baf0d523"
-
-            $.ajax({
-                method: "GET",
-                url: uvUrl,
-                data: 'json',
-                success: (response) => {
-                    console.log(response);
-                    $(".uv").html("UV: " + response.value);
-                }
-            });
-
-            $.ajax({
-                method: "GET",
-                url: fiveDayUrl,
-                data: 'json',
-                success: (response) => {
-                    console.log(response);
-                    const forecastDays = response.daily;
-
-                    jQuery.each(forecastDays, function(index, value) {
-                        const forecastTimestamp = value.dt;
-                        const forecastDate = new Date(forecastTimestamp * 1000);
-                        const forecastMonth = (forecastDate.getMonth() + 1);
-                        const forecastDay = (forecastDate.getDate());
-                        const forecastYear = (forecastDate.getFullYear());
-                        const formattedDay = forecastMonth + '/' + forecastDay + '/' + forecastYear;
-
-                        if (index < 6 && index > 0) {
-                            console.log(formattedDay);
-                            
-                        }
-                    });
-                }
-            });
-        }
+    jQuery.each(searches, function(index, value) {
+        $('.saved-searches').append(`
+            <li>` + value + `</li>
+        `);
     });
+
+
+
+
+    $('button').click(function(e) {
+        e.preventDefault();
+        $(".five-day").empty();
+        const city = $('input').val();
+        localStorage.setItem('city-' + city, city);
+
+        const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=c2a1f6a8ab689e2fe8baffb9baf0d523";
+
+        $.ajax({
+            method: "GET",
+            url: weatherUrl,
+            data: 'json',
+            success: (response) => {
+                const icon = response.weather[0].icon;
+                const iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
+                const timestamp = response.dt;
+                const date = new Date(timestamp * 1000);
+                const month = (date.getMonth() + 1);
+                const day = (date.getDate());
+                const year = (date.getFullYear());
+                const currentDay = month + '/' + day + '/' + year;
+
+                $(".city").html(response.name + ' ' + currentDay + "<img src=" + iconurl + ">");
+                $(".temp").html("Temperature: " + response.main.temp + " &#8457");
+                $(".humidity").html("Humidity: " + response.main.humidity + "%");
+                $(".wind-speed").html("Wind Speed: " + response.wind.speed);
+
+                const lat = response.coord.lat;
+                const lon = response.coord.lon;
+                const uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=c2a1f6a8ab689e2fe8baffb9baf0d523";
+                const fiveDayUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&units=imperial&appid=c2a1f6a8ab689e2fe8baffb9baf0d523"
+
+                $.ajax({
+                    method: "GET",
+                    url: uvUrl,
+                    data: 'json',
+                    success: (response) => {
+                        $(".uv").html("UV Index: <span>" + response.value + "</span>");
+
+                        if (response.value < 3) {
+                            $(".uv span").addClass('low');
+                        } else if (response.value > 2 && response.value < 6) {
+                            $(".uv span").addClass('moderate');
+                        } else if (response.value > 5) {
+                            $(".uv span").addClass('high');
+                        }
+                    }
+                });
+
+                $.ajax({
+                    method: "GET",
+                    url: fiveDayUrl,
+                    data: 'json',
+                    success: (response) => {
+                        const forecastDays = response.daily;
+
+                        jQuery.each(forecastDays, function(index, value) {
+                            const forecastTimestamp = value.dt;
+                            const forecastDate = new Date(forecastTimestamp * 1000);
+                            const forecastMonth = (forecastDate.getMonth() + 1);
+                            const forecastDay = (forecastDate.getDate());
+                            const forecastYear = (forecastDate.getFullYear());
+                            const formattedDay = forecastMonth + '/' + forecastDay + '/' + forecastYear;
+
+                            const temp = value.temp.day;
+                            const humidity = value.humidity;
+                            const weather = value.weather[0].icon;
+
+                            if (index < 6 && index > 0) {
+                                $(".five-day").append(`
+                                    <li>
+                                        <p>` + formattedDay + `</p>
+                                        <img src="http://openweathermap.org/img/w/` + weather + `.png"/>
+                                        <p>Temp: ` + temp + ` &#8457</p>
+                                        <p>Humidity: ` + humidity + `%</p>
+                                    </li>
+                                `);
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    });
+
 });
